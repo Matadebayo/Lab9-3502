2fdfb459d836d039d3496b22988e63c68d290706
Aspangler4804 <aspangler1@ufl.edu>
2024-04-10 11:07:36 -0400

    Push 4

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #Matthew Adebayo
-message = str(input())
+
 def encode(ori_number):
     finished = ""
     for num in ori_number:
@@ -7,4 +7,44 @@ def encode(ori_number):
         finished += str(new)
     return print(finished)
 encode(message)
+
+def main():
+    while True:
+        print("Menu")
+        print("_____________")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        userchoice = input("Please enter an option:")
+        if userchoice == 1:
+            message = (input("Please enter an 8 digit password:"))
+
+            try:
+                if len(message) == 8:
+                    break
+                else:
+                    print("Please enter your password to encode:")
+            except ValueError:
+                print("Please enter a valid password")
+            print("Your password has been encoded and stored!")
+            encode(message)
+        if userchoice == 2:
+            Decode(message)
+
+
+def Decode():
+    DecodedPswrd = ""
+    for i in finished:
+        update = int(i) - 3
+        if update > 0:
+            update = str(update)
+        else:
+            if update == 0:
+                update = "7"
+            if update == 1:
+                update = "8"
+            if update == 2:
+                update = "9"
+        DecodedPswrd += (update)
+    print(DecodedPswrd)
 
