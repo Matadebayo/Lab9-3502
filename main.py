411728863d31e0fa3715975244ae655b5866f7b6
Matadebayo <Madebayo@ufl.edu>
2024-04-10 10:29:14 -0400

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello world")
+#Matthew Adebayo
+message = str(input())
+def encode(ori_number):
+    for i in range(len(ori_number)):
+        new = int(ori_number[i]) += 3
+        print(new, End="")
+    return
+encode(message)
